514f1aa2a9e7398927818dfd97c695bb820df799
ToyamaHideki <toyama.hideki.bns@gmail.com>
2022-01-07 09:20:42 +0900

    設計書(doc)追加

--- Todo-app/public/js/newtask.js
+++ Todo-app/public/js/newtask.js
@@ -2,12 +2,19 @@
 function disp(){
 
 
-	number = window.prompt("作成日を記入してください", "例2022-01-01");
+	number = window.prompt("作成日を記入してください", "20220101");
+    result = number.match( /(\d{4})(\d{2})(\d{2})/ );
 
-if(number != "" && number != null && number == /^[0-9]{4}-?[0-9]{2}-?[0-9]{2}$/){
+if(number != "" && number != null){
 		window.alert('OK');
 	}else{
 		window.alert('正しい値を入力してください');
 	}
+
+  if(  result == true){
+    window.alert('OK');
+  }else{
+      window.alert("No");
+  }
 
 }
